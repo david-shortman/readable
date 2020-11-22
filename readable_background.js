a6818100cf29a118bf56249887f1094ec63271a2
David <tim.apple@mailfence.com>
2020-11-21 20:46:57 -0500

    browser compatability

--- readable_background.js
+++ readable_background.js
@@ -1,10 +1,21 @@
 const browser = window['browser'] || chrome;
 
+function onGotCurrentTab(currentTabOrTabs) {
+    let currentTabId;
+    if (Array.isArray(currentTabOrTabs)) {
+        currentTabId = currentTabOrTabs[0].id;
+    } else {
+        currentTabId = currentTabOrTabs.id;
+    }
+    if (currentTabId >= 0) {
+        browser.tabs.sendMessage(currentTabId, "updateCurrentTab");
+    }
+}
+
 setInterval(function () {
-    browser.tabs.getSelected(function (currentTab) {
-        console.log(currentTab);
-        if (currentTab.id >= 0) {
-            browser.tabs.sendMessage(currentTab.id, "updateCurrentTab");
-        }
-    });
+    if (browser.tabs.getSelected) {
+        browser.tabs.getSelected(onGotCurrentTab);
+    } else {
+        browser.tabs.query({ active: true }).then(onGotCurrentTab)
+    }
 }, 5000);
